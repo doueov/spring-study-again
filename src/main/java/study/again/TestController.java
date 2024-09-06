7a1d0c7544ba46eb36eef14d6f97485222d51714
doueov <gitpdyeon@gmail.com>
2024-09-06 09:55:28 +0900

    2024-09-06

--- src/main/java/study/again/TestController.java
+++ src/main/java/study/again/TestController.java
@@ -20,6 +20,11 @@ public class TestController {
         return responseData;
     }
 
+    // 1. JSON 데이터(문자열)이 도착
+    // 2. (데이터컨버터) 내부적으로 Jackson 라이브버리를 활용하여 JSON데이터(문자열)에서 내용을 추출
+    // 3. 객체를 생성 => CalculatorResult로 추출
+    // 4. 객체 내용을 채움 (세터 메서드) => "num", "pow" => setNum, setPow를 호출해서 내용 지우기
+    // 5. 객체를 메서드 핸들러의 파라미터로 전달
     @PostMapping("/power")
     @ResponseBody
     public CalculatorResult power(@RequestBody Calculator cal) {
